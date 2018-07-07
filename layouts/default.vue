967c182aa3521dfc6276d38674687d4c823b8130
AGD <agdholo@gmail.com>
2018-07-07 17:10:39 +0800

    init steamhub

---
+++ layouts/default.vue
@@ -0,0 +1,91 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      clipped
+      class="grey lighten-4"
+      app
+    >
+      <v-list
+        dense
+        class="grey lighten-4"
+      >
+        <template v-for="(item, i) in items">
+          <v-layout
+            v-if="item.heading"
+            :key="i"
+            row
+            align-center
+          >
+            <v-flex xs6>
+              <v-subheader v-if="item.heading">
+                {{ item.heading }}
+              </v-subheader>
+            </v-flex>
+          </v-layout>
+          <v-divider
+            v-else-if="item.divider"
+            :key="i"
+            dark
+            class="my-3"
+          ></v-divider>
+          <v-list-tile
+            v-else
+            :key="i"
+            :to=item.link
+          >
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title class="grey--text">
+                {{ item.text }}
+              </v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar color="purple" dark app fixed clipped-left>
+      <v-toolbar-side-icon @click.native="drawer = !drawer"></v-toolbar-side-icon>
+      <span class="title ml-3 mr-5">SteamHub</span>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height class="grey lighten-4">
+        <nuxt />
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: null,
+      items: [
+        { icon: 'home', text: '主页', link: '/' },
+        { divider: true },
+        { heading: '游戏' },
+        { icon: 'list', text: '列表', link: '/steam/game/list' },
+        { divider: true }
+      ]
+    }),
+    props: {
+      source: String
+    }
+  }
+</script>
+
+<style>
+  #keep main .container {
+    height: 660px;
+  }
+  .v-navigation-drawer__border {
+    display: none;
+  }
+  .text {
+    font-weight: 400;
+  }
+  </style>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-layout justify-center align-center class="text-xs-center">
+    <v-flex xs12 sm8 md6>
+      <v-card
+        color="primary"
+        dark
+      >
+        <v-card-text>
+          欢迎来到 SteamHub
+          <v-progress-linear
+            indeterminate
+            color="white"
+            class="mb-0"
+          ></v-progress-linear>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
