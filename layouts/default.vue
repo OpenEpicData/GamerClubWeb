4e22dc2df784fbfee9fa336eec0441cd985a4b09
AGD <agdholo@gmail.com>
2018-07-08 13:07:46 +0800

    delete drawer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,93 +1,66 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      fixed
-      clipped
-      class="grey lighten-4 toolbar-drawer"
-      app
-    >
-      <v-list
-        dense
-        class="grey lighten-4"
-      >
-        <template v-for="(item, i) in items">
-          <v-layout
-            v-if="item.heading"
-            :key="i"
-            row
-            align-center
-          >
-            <v-flex xs6>
-              <v-subheader v-if="item.heading">
-                {{ item.heading }}
-              </v-subheader>
-            </v-flex>
-          </v-layout>
-          <v-divider
-            v-else-if="item.divider"
-            :key="i"
-            dark
-            class="my-3"
-          ></v-divider>
-          <v-list-tile
-            v-else
-            :key="i"
-            :to=item.link
-          >
-            <v-list-tile-action>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title class="grey--text">
-                {{ item.text }}
-              </v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </template>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar app fixed clipped-left>
-      <v-toolbar-side-icon @click.native="drawer = !drawer" class="toolbar-drawer-icon"></v-toolbar-side-icon>
+    <v-toolbar app fixed clipped-left class="white">
       <span class="title ml-3 mr-5">SteamHub</span>
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn flat>浏览</v-btn>
+        <v-btn flat to="/">浏览</v-btn>
         <v-btn flat>趋势</v-btn>
       </v-toolbar-items>
       <v-text-field
-        class="mt-2"
+        class="mt-2 ml-1"
         solo-inverted
-        label="搜索其实很简单"
+        label="搜索暂不可用"
         append-icon="search"
       ></v-text-field>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn flat>加入社区 <v-icon right>add_circle_outline</v-icon></v-btn>
+        <v-btn 
+        flat
+        :disabled="dialog"
+        @click.stop="dialog = true"
+        >
+          加入社区 <v-icon right>add_circle_outline</v-icon></v-btn>
       </v-toolbar-items>
     </v-toolbar>
     <v-content class="grey lighten-4">
-      <v-container fluid>
-        <nuxt />
-      </v-container>
+      <nuxt />
     </v-content>
+    <v-dialog
+      v-model="dialog"
+      hide-overlay
+      persistent
+      width="300"
+    >
+      <v-card
+        color="primary"
+        dark
+      >
+        <v-card-text>
+          功能开发中
+          <v-progress-linear
+            indeterminate
+            color="white"
+            class="mb-0"
+          ></v-progress-linear>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
   </v-app>
 </template>
 
 <script>
   export default {
     data: () => ({
-      drawer: false,
-      items: [
-        { icon: 'home', text: '主页', link: '/' },
-        { divider: true },
-        { heading: '游戏' },
-        { icon: 'list', text: '列表', link: '/steam/game/list' },
-        { icon: 'search', text: '搜索', link: '/steam/game/search' },
-        { divider: true }
-      ]
+      dialog: false
     }),
     props: {
       source: String
+    },
+    watch: {
+      dialog (val) {
+        if (!val) return
+        setTimeout(() => (this.dialog = false), 1000)
+      }
     }
   }
 </script>
