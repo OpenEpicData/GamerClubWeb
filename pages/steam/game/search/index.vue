c7b9aa16ac58fb302b1403ec924b989b2a9a4fb7
AGD <agdholo@gmail.com>
2018-07-08 13:08:34 +0800

    add search page

---
+++ pages/steam/game/search/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-layout justify-center align-center class="text-xs-center">
+    <div>
+      <h1 class="display-2">SteamHub</h1>
+    </div>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+    })
+  }
+</script>
