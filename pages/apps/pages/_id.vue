01c4f6add82317522c7dfb910703680a01e3a40b
AGD <agdholo@gmail.com>
2018-07-11 22:29:03 +0800

    create applist card

---
+++ pages/apps/pages/_id.vue
@@ -0,0 +1,172 @@
+<template>
+  <div id="GameList">
+    <v-container grid-list-sm>
+      <v-layout row wrap>
+        <v-flex xs8>
+          <v-btn color="deep-purple lighten-1" dark>
+            <v-icon left class="mt-1">apps</v-icon>游戏
+          </v-btn>
+          <h4 class="mt-2">记录在案的游戏: {{ list.total }}, 数据采集中...</h4>
+        </v-flex>
+        <v-flex xs4>
+          <div class="text-xs-right mt-3">
+            <h3>第 {{ page }} 页</h3>
+          </div>
+        </v-flex>
+      </v-layout>
+      
+      <v-layout row wrap class="mt-3">
+        <v-flex xs12 sm6 md6 lg4 xl3 v-for="(item,i) in list.data" :key="i">
+          <v-card :to="'/apps/' + item.AppID" v-if="item.AppType">
+            <v-card-media :src="'https://steamcdn-a.opskins.media/steam/apps/' + item.AppID + '/header.jpg'" height="200px">
+              <v-container fill-height fluid pa-2>
+                <v-layout fill-height>
+                  <v-flex xs12 align-end flexbox class="text-xs-right" v-if="item.AppsTypes">
+                    <v-btn dark color="deep-purple lighten-1">
+                      <v-icon left class="mt-1">{{ item.AppsTypes.DisplayName | typeIcon }}</v-icon> {{ item.AppsTypes.DisplayName | typeName }} </v-btn>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-card-media>
+            <v-card-title primary-title>
+              <div>
+                <h3 class="headline mb-0">
+                  <v-chip>
+                    {{ item.AppID }}
+                  </v-chip>
+                  <v-btn>
+                    {{ item.Name.slice(0, 20) }}
+                  </v-btn>
+                </h3>
+                <h5 class="ml-3">更新于: {{ item.LastUpdated | time }}</h5>
+              </div>
+            </v-card-title>
+
+          </v-card>
+          <v-card v-else>
+            <v-card-media :src="'/unknow.jpg'" height="200px">
+              <v-container fill-height fluid pa-2>
+                  <v-layout fill-height>
+                    <v-flex xs12 align-end flexbox class="text-xs-right">
+                      <v-btn dark color="red">
+                        <v-icon left class="mt-1">device_unknown</v-icon> 未知 </v-btn>
+                    </v-flex>
+                  </v-layout>
+                </v-container>
+            </v-card-media>
+            <v-card-title primary-title>
+              <div>
+                <h3 class="headline mb-0">
+                  <v-chip>
+                    {{ item.AppID }}
+                  </v-chip>
+                  <v-btn>
+                    未知应用
+                  </v-btn>
+                </h3>
+                <h5 class="ml-3">更新于: {{ item.LastUpdated | time }}</h5>
+              </div>
+            </v-card-title>
+          </v-card>
+        </v-flex>
+      </v-layout>
+      <v-layout>
+        <v-pagination
+          v-model="page"
+          :length=list.lastPage
+        ></v-pagination>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+  import relativeTime from 'dayjs/plugin/relativeTime'
+  import dayjs from 'dayjs'
+
+  import 'dayjs/locale/zh-cn'
+  dayjs.extend(relativeTime)
+
+  export default {
+    async asyncData ({ params }) {
+      return axios.get(`http://localhost:3333/api/v1/steam/apps?list=` + params.id, {
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        }
+      })
+        .then(function (response) {
+          let page = parseInt(response.data.page)
+          if (params.id === undefined) {
+            page = 1
+          }
+          return { list: response.data, page: page }
+        })
+    },
+    data: () => ({
+      page: 1
+    }),
+    methods: {
+
+    },
+    watch: {
+      page: function (newPage, oldPage) {
+        this.$vuetify.goTo('#GameList', 'easyInQuad')
+        this.$router.push({path: '/apps/pages/' + newPage})
+        return axios.get(`http://localhost:3333/api/v1/steam/apps?list=` + newPage, {
+          headers: {
+            'Access-Control-Allow-Origin': '*'
+          }
+        })
+          .then(response => {
+            this.list = response.data
+          })
+      }
+    },
+    filters: {
+      time: function (value) {
+        return dayjs().locale('zh-cn').from(dayjs(value))
+      },
+      typeName: function (value) {
+        switch (value) {
+          case 'Game':
+            return '游戏'
+          case 'Tool':
+            return '工具'
+          case 'Video':
+            return '影音'
+          case 'Application':
+            return '应用'
+          case 'Demo':
+            return '试玩版'
+          case 'DLC':
+            return '扩充包'
+          case 'Hardware':
+            return '硬件'
+          default:
+            return '未知'
+        }
+      },
+      typeIcon: function (value) {
+        switch (value) {
+          case 'Game':
+            return 'games'
+          case 'Tool':
+            return 'build'
+          case 'Video':
+            return 'videocam'
+          case 'Application':
+            return 'apps'
+          case 'Demo':
+            return 'play_circle_outline'
+          case 'DLC':
+            return 'playlist_add'
+          case 'Hardware':
+            return 'computer'
+          default:
+            return 'device_unknown'
+        }
+      }
+    }
+  }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <div class="white">
+    <div>
       <v-container hidden-sm-and-down>
-        <v-layout row wrap class="py-5">
+        <v-layout row wrap class="pb-5">
           <v-flex xs10>
             <div class="text-xs-left">
               <h1 class="display-2 font-weight-bold">全球 Steam 数据统计分析 By SteamHub</h1>
@@ -28,9 +28,10 @@
         </v-layout>
       </v-container>
     </div>
-    <div>
+    <div class="grey lighten-4">
       <v-container grid-list-sm>
         <div class="hidden-sm-and-down">
+          <v-progress-linear indeterminate color="blue" class="mb-0" :height="carouselLoading"></v-progress-linear>
           <el-carousel :interval="4000" type="card">
             <el-carousel-item v-for="item in feature" :key="item.id" class="text-xs-center">
               <img :src="item.large_capsule_image" alt="" v-on:click="carouselTo(item.id)">
@@ -55,28 +56,16 @@
         </v-layout>
         <v-layout row wrap class="mt-3">
           <v-flex xs12 sm6 md6 lg4 xl3 v-for="(item,i) in list.data" :key="i">
-            <v-card :to="'/apps/' + item.AppID">
-              <v-card-media
-                :src="'https://steamcdn-a.opskins.media/steam/apps/' + item.AppID + '/header.jpg'"
-                height="200px"
-              >
-              <v-container
-                fill-height
-                fluid
-                pa-2
-              >
-                <v-layout fill-height>
-                  <v-flex xs12 align-end flexbox class="text-xs-right">
-                    <v-btn dark color="deep-purple lighten-1" v-if="item.AppsTypes.DisplayName === 'Game'"><v-icon left class="mt-1">games</v-icon> 游戏 </v-btn>
-                    <v-btn color="info" v-if="item.AppsTypes.DisplayName === 'Tool'"><v-icon left class="mt-1">build</v-icon> 工具 </v-btn>
-                    <v-btn color="info" v-if="item.AppsTypes.DisplayName === 'Video'"><v-icon left class="mt-1">videocam</v-icon> 影音 </v-btn>
-                    <v-btn color="info" v-if="item.AppsTypes.DisplayName === 'Application'"><v-icon left class="mt-1">apps</v-icon> 应用 </v-btn>
-                    <v-btn color="info" v-if="item.AppsTypes.DisplayName === 'Demo'"><v-icon left class="mt-1">play_circle_outline</v-icon> 试玩版 </v-btn>
-                    <v-btn color="info" v-if="item.AppsTypes.DisplayName === 'DLC'"><v-icon left class="mt-1">playlist_add</v-icon> 扩充包 </v-btn>
-                    <v-btn color="info" v-if="item.AppsTypes.DisplayName === 'Hardware'"><v-icon left class="mt-1">computer</v-icon> 硬件 </v-btn>
-                  </v-flex>
-                </v-layout>
-              </v-container>
+            <v-card :to="'/apps/' + item.AppID" v-if="item.AppType">
+              <v-card-media :src="'https://steamcdn-a.opskins.media/steam/apps/' + item.AppID + '/header.jpg'" height="200px">
+                <v-container fill-height fluid pa-2>
+                  <v-layout fill-height>
+                    <v-flex xs12 align-end flexbox class="text-xs-right" v-if="item.AppsTypes">
+                      <v-btn dark color="deep-purple lighten-1">
+                        <v-icon left class="mt-1">{{ item.AppsTypes.DisplayName | typeIcon }}</v-icon> {{ item.AppsTypes.DisplayName | typeName }} </v-btn>
+                    </v-flex>
+                  </v-layout>
+                </v-container>
               </v-card-media>
               <v-card-title primary-title>
                 <div>
@@ -88,22 +77,39 @@
                       {{ item.Name.slice(0, 20) }}
                     </v-btn>
                   </h3>
+                  <h5 class="ml-3">更新于: {{ item.LastUpdated | time }}</h5>
                 </div>
               </v-card-title>
 
             </v-card>
-          </v-flex>
-          <v-flex xs6 class="mb-5">
-            <div class="text-xs-left" v-if="page !== 1">
-              <v-btn large round outline v-on:click="fetchPage(page - 1)"> 上一页 {{ page - 1 }}</v-btn>
-            </div>
-          </v-flex>
-          <v-flex xs6 class="mb-5">
-            <div class="text-xs-right" v-if="page !== list.total">
-              <v-btn large round outline v-on:click="fetchPage(page + 1)"> 下一页 {{ page + 1 }}</v-btn>
-            </div>
+            <v-card v-else>
+              <v-card-media :src="'/unknow.jpg'" height="200px">
+                <v-container fill-height fluid pa-2>
+                    <v-layout fill-height>
+                      <v-flex xs12 align-end flexbox class="text-xs-right">
+                        <v-btn dark color="red">
+                          <v-icon left class="mt-1">device_unknown</v-icon> 未知 </v-btn>
+                      </v-flex>
+                    </v-layout>
+                  </v-container>
+              </v-card-media>
+              <v-card-title primary-title>
+                <div>
+                  <h3 class="headline mb-0">
+                    <v-chip>
+                      {{ item.AppID }}
+                    </v-chip>
+                    <v-btn>
+                      未知应用
+                    </v-btn>
+                  </h3>
+                  <h5 class="ml-3">更新于: {{ item.LastUpdated | time }}</h5>
+                </div>
+              </v-card-title>
+            </v-card>
           </v-flex>
         </v-layout>
+
       </v-container>
     </div>
   </div>
@@ -121,10 +127,15 @@
 
 <script>
   import axios from 'axios'
+  import relativeTime from 'dayjs/plugin/relativeTime'
+  import dayjs from 'dayjs'
 
+  import 'dayjs/locale/zh-cn'
+  dayjs.extend(relativeTime)
+  
   export default {
     async asyncData () {
-      return axios.get(`http://localhost:3333/api/v1/steam/apps`, {
+      return axios.get(`http://localhost:3333/api/v1/steam/apps?page=1`, {
         headers: {
           'Access-Control-Allow-Origin': '*'
         }
@@ -139,24 +150,12 @@
       step: '0',
       isPrice: '',
       queue: '',
-      feature: ''
+      feature: '',
+      carouselLoading: 6
     }),
     methods: {
       carouselTo: function (id) {
         this.$router.push({path: '/apps/' + id})
-      },
-      fetchPage: function (page) {
-        this.$vuetify.goTo('#today', 'easyInQuad')
-        return axios.get(`http://localhost:3333/api/v1/steam/apps?page=` + page, {
-          headers: {
-            'Access-Control-Allow-Origin': '*'
-          }
-        })
-          .then(response => {
-            this.list = response.data
-            this.page = page++
-            this.price = ''
-          })
       }
     },
     created: function () {
@@ -178,12 +177,58 @@
       })
         .then(response => {
           this.feature = response.data.specials.items
+          this.carouselLoading = 0
         })
     },
     watch: {
       dialog2 (val) {
         if (!val) return
         setTimeout(() => (this.dialog2 = false), 1000)
+      }
+    },
+    filters: {
+      time: function (value) {
+        return dayjs().locale('zh-cn').from(dayjs(value))
+      },
+      typeName: function (value) {
+        switch (value) {
+          case 'Game':
+            return '游戏'
+          case 'Tool':
+            return '工具'
+          case 'Video':
+            return '影音'
+          case 'Application':
+            return '应用'
+          case 'Demo':
+            return '试玩版'
+          case 'DLC':
+            return '扩充包'
+          case 'Hardware':
+            return '硬件'
+          default:
+            return '未知'
+        }
+      },
+      typeIcon: function (value) {
+        switch (value) {
+          case 'Game':
+            return 'games'
+          case 'Tool':
+            return 'build'
+          case 'Video':
+            return 'videocam'
+          case 'Application':
+            return 'apps'
+          case 'Demo':
+            return 'play_circle_outline'
+          case 'DLC':
+            return 'playlist_add'
+          case 'Hardware':
+            return 'computer'
+          default:
+            return 'device_unknown'
+        }
       }
     }
   }
