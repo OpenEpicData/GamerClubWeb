d45791cbe7b10bac295e10a39a51b896118b9d91
AGD <agdholo@gmail.com>
2018-07-11 18:07:03 +0800

    add carousel

--- pages/index.vue
+++ pages/index.vue
@@ -30,23 +30,62 @@
     </div>
     <div>
       <v-container grid-list-sm>
-        <v-btn color="deep-purple lighten-1" dark>
-          <v-icon left class="mt-1">blur_on</v-icon>趋势</v-btn>
-        <h2 class="mt-3">今日
-          <v-chip>{{ month }}月{{ day }}日</v-chip>
-        </h2>
-        <h4 class="mt-2">记录在案的游戏: {{ list.total }}, 每日第十二小时更新</h4>
+        <div class="hidden-sm-and-down">
+          <el-carousel :interval="4000" type="card">
+            <el-carousel-item v-for="item in feature" :key="item.id" class="text-xs-center">
+              <img :src="item.large_capsule_image" alt="" v-on:click="carouselTo(item.id)">
+            </el-carousel-item>
+          </el-carousel>
+        </div>
+        <v-layout row wrap id="today">
+          <v-flex xs8>
+            <v-btn color="deep-purple lighten-1" dark>
+              <v-icon left class="mt-1">blur_on</v-icon>趋势
+            </v-btn>
+            <h2 class="mt-3">今日
+              <v-chip>{{ month }}月{{ day }}日</v-chip>
+            </h2>
+            <h4 class="mt-2">记录在案的游戏: {{ list.total }}, 数据采集中...</h4>
+          </v-flex>
+          <v-flex xs4>
+            <div class="text-xs-right mt-3">
+              <h3>更新队列中的项目: {{ queue }} 个</h3>
+            </div>
+          </v-flex>
+        </v-layout>
         <v-layout row wrap class="mt-3">
           <v-flex xs12 sm6 md6 lg4 xl3 v-for="(item,i) in list.data" :key="i">
-            <v-card>
+            <v-card :to="'/apps/' + item.AppID">
+              <v-card-media
+                :src="'https://steamcdn-a.opskins.media/steam/apps/' + item.AppID + '/header.jpg'"
+                height="200px"
+              >
+              <v-container
+                fill-height
+                fluid
+                pa-2
+              >
+                <v-layout fill-height>
+                  <v-flex xs12 align-end flexbox class="text-xs-right">
+                    <v-btn dark color="deep-purple lighten-1" v-if="item.AppsTypes.DisplayName === 'Game'"><v-icon left class="mt-1">games</v-icon> 游戏 </v-btn>
+                    <v-btn color="info" v-if="item.AppsTypes.DisplayName === 'Tool'"><v-icon left class="mt-1">build</v-icon> 工具 </v-btn>
+                    <v-btn color="info" v-if="item.AppsTypes.DisplayName === 'Video'"><v-icon left class="mt-1">videocam</v-icon> 影音 </v-btn>
+                    <v-btn color="info" v-if="item.AppsTypes.DisplayName === 'Application'"><v-icon left class="mt-1">apps</v-icon> 应用 </v-btn>
+                    <v-btn color="info" v-if="item.AppsTypes.DisplayName === 'Demo'"><v-icon left class="mt-1">play_circle_outline</v-icon> 试玩版 </v-btn>
+                    <v-btn color="info" v-if="item.AppsTypes.DisplayName === 'DLC'"><v-icon left class="mt-1">playlist_add</v-icon> 扩充包 </v-btn>
+                    <v-btn color="info" v-if="item.AppsTypes.DisplayName === 'Hardware'"><v-icon left class="mt-1">computer</v-icon> 硬件 </v-btn>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+              </v-card-media>
               <v-card-title primary-title>
                 <div>
                   <h3 class="headline mb-0">
                     <v-chip>
-                      {{ item.appid }}
+                      {{ item.AppID }}
                     </v-chip>
-                    <v-btn v-on:click="fetchPrice(item.appid)" @click.stop="dialogPrice = true">
-                      {{ item.name.slice(0,20) }}
+                    <v-btn>
+                      {{ item.Name.slice(0, 20) }}
                     </v-btn>
                   </h3>
                 </div>
@@ -65,54 +104,27 @@
             </div>
           </v-flex>
         </v-layout>
-        <v-dialog v-model="dialogPrice" hide-overlay v-if="isPrice === 1">
-          <div>
-            <v-stepper v-model="step" vertical non-linear v-for="(price, index) in price" :key="index">
-              <v-stepper-step :step="index" complete editable>
-                时间: {{ price.created_at }}
-              </v-stepper-step>
-
-              <v-stepper-content :step="index">
-                <del>原价: {{ price.initial/100 }}</del> 现价: {{ price.final/100 }}
-                <v-chip outline color="primary" v-if="price.discount_percent === 0">没有折扣</v-chip>
-                <v-chip outline color="red" v-else>打折中: {{ price.discount_percent }}</v-chip>
-              </v-stepper-content>
-
-            </v-stepper>
-          </div>
-        </v-dialog>
-        <v-dialog
-          v-model="dialogPrice"
-          hide-overlay
-          width="300"
-          v-if="isPrice === 0"
-        >
-          <v-card
-            color="primary"
-            dark
-          >
-            <v-card-text>
-              暂无数据或是免费游戏
-              <v-progress-linear
-                indeterminate
-                color="white"
-                class="mb-0"
-                height="1"
-              ></v-progress-linear>
-            </v-card-text>
-          </v-card>
-        </v-dialog>
       </v-container>
     </div>
   </div>
 </template>
+
+<style>
+  .el-carousel__item:nth-child(2n) {
+    background-color: transparent
+  }
+  
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: transparent
+  }
+</style>
 
 <script>
   import axios from 'axios'
 
   export default {
     async asyncData () {
-      return axios.get(`https://api.steamhub.cn/api/v1/steam/game/lists`, {
+      return axios.get(`http://localhost:3333/api/v1/steam/apps`, {
         headers: {
           'Access-Control-Allow-Origin': '*'
         }
@@ -123,30 +135,19 @@
     },
     data: () => ({
       dialog2: false,
-      dialogPrice: false,
-      dialogPriceClose: false,
       price: '',
       step: '0',
-      isPrice: ''
+      isPrice: '',
+      queue: '',
+      feature: ''
     }),
     methods: {
-      fetchPrice (appid) {
-        return axios.get(`https://api.steamhub.cn/api/v1/steam/game/searches/?appid=` + appid + `&filter=priceTimeDesc`, {
-          headers: {
-            'Access-Control-Allow-Origin': '*'
-          }
-        })
-          .then(response => {
-            if (response.data[0] === undefined) {
-              this.isPrice = 0
-            } else {
-              this.isPrice = 1
-            }
-            this.price = response.data
-          })
+      carouselTo: function (id) {
+        this.$router.push({path: '/apps/' + id})
       },
       fetchPage: function (page) {
-        return axios.get(`https://api.steamhub.cn/api/v1/steam/game/lists?page=` + page, {
+        this.$vuetify.goTo('#today', 'easyInQuad')
+        return axios.get(`http://localhost:3333/api/v1/steam/apps?page=` + page, {
           headers: {
             'Access-Control-Allow-Origin': '*'
           }
@@ -162,6 +163,22 @@
       const Today = new Date()
       this.month = Today.getMonth() + 1
       this.day = Today.getDate()
+      axios.get(`http://localhost:3333/api/v1/steam/app/store/update/queues`, {
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        }
+      })
+        .then(response => {
+          this.queue = response.data.total
+        })
+      axios.get(`http://localhost:3333/api/v1/steam/game/features`, {
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        }
+      })
+        .then(response => {
+          this.feature = response.data.specials.items
+        })
     },
     watch: {
       dialog2 (val) {
