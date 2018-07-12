e8f036dd1ac973f162cc01632850e6341f53a900
AGD <agdholo@gmail.com>
2018-07-12 20:27:45 +0800

    add flex to game list card

--- pages/apps/pages/_id.vue
+++ pages/apps/pages/_id.vue
@@ -15,7 +15,42 @@
         </v-flex>
       </v-layout>
       
-      <GameListCard :list.sync="list"></GameListCard>
+      <v-layout row wrap class="mt-3" id="GameList">
+          <GameListCard :list.sync="list"></GameListCard>
+          <v-flex d-flex lg3>
+            <v-layout row wrap hidden-md-and-down>
+              <v-flex d-flex>
+                <v-layout row wrap>
+                  <v-flex xs12>
+                    <v-card color="deep-purple lighten-1 mb-5" flat dark>
+                      <v-card-title primary-title>
+                        <div>
+                          <h4 class="headline mb-0"><v-icon left>edit</v-icon> 加入创作挑战</h4>
+                        </div>
+                      </v-card-title>
+                      <v-card-text>
+                        <span>是时候投票选出你最喜欢的文章了</span>
+                      </v-card-text>
+                      <v-card-actions class="ml-1 pb-3">
+                        <v-btn color="white" class="black--text">
+                          立即开始
+                        </v-btn>
+                      </v-card-actions>
+                    </v-card>
+
+                    <v-card color="grey lighten-5" flat>
+                      <v-card-title primary-title>
+                        <div>
+                          <h4 class="headline mb-0"><v-icon left>attach_money</v-icon> 成为赞助商 <v-icon right>chevron_right</v-icon></h4>
+                        </div>
+                      </v-card-title>
+                    </v-card>
+                  </v-flex>
+                </v-layout>
+              </v-flex>
+            </v-layout>
+          </v-flex>
+        </v-layout>
       <v-layout>
         <v-pagination
           v-model="page"
