6441c61b249f3b0ae33008f416314b6d85a6ccf1
AGD <agdholo@gmail.com>
2018-07-12 11:18:42 +0800

    add new logo

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,10 +8,13 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1.0, user-scalable=no' },
-      { hid: 'description', name: 'description', content: 'SteamHub 是一个全球 Steam 的数据统计社区,每天为开发者和玩家提供实时的 价格,资讯 数据查询 ' }
+      { hid: 'description', name: 'description', content: 'SteamHub 是一个全球 Steam 的数据统计社区,每天为开发者和玩家提供实时的 价格,资讯 数据查询 ' },
+      { 'http-equiv': 'pragma', content: 'no-cache' },
+      { 'http-equiv': 'cache-control', content: 'no-cache' },
+      { 'http-equiv': 'expires', content: '0' }
     ],
     link: [
-      { rel: 'icon', type: 'image/*', href: '/logo.jpg' },
+      { rel: 'icon', type: 'image/*', href: '/logo_64x64.png' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
     ]
   },
