9fba639a94c9e6f190b9b85361b5140b3cab601d
AGD <agdholo@gmail.com>
2018-07-12 15:02:47 +0800

    fix for dialog bug

--- components/GameListCard.vue
+++ components/GameListCard.vue
@@ -58,15 +58,15 @@
           </div>
         </v-card-title>
       </v-card>
-      <v-dialog v-model="dialogAttention" hide-overlay persistent width="300">
-          <v-card color="primary" dark>
-            <v-card-text>
-              功能开发中
-              <v-progress-linear indeterminate color="white" class="mb-0"></v-progress-linear>
-            </v-card-text>
-          </v-card>
-      </v-dialog>
     </v-flex>
+    <v-dialog v-model="dialogAttention" hide-overlay persistent width="300">
+        <v-card color="primary" dark>
+          <v-card-text>
+            功能开发中
+            <v-progress-linear indeterminate color="white" class="mb-0"></v-progress-linear>
+          </v-card-text>
+        </v-card>
+    </v-dialog>
   </v-layout>
 </template>
 
