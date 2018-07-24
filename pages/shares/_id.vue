70ee621ac2f07c4ed996a7f27c58ae7460e7be8e
AGD <agdholo@gmail.com>
2018-07-24 15:33:45 +0800

    create share page

---
+++ pages/shares/_id.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-jumbotron>
+    <v-card>
+      <v-container fill-height>
+        <v-layout align-center>
+          <v-flex xs12 sm12 md12>
+            <div v-for="(item, i) in appdetails" :key="i">
+              <h3 class="display-1 mb-3">{{ item.data.name }} 的数据分析,价格走势</h3>
+
+              <span class="subheading">{{ item.data.short_description }}</span>
+            </div>
+            <v-divider class="my-2"></v-divider>
+
+            <div class="text-xs-right">
+              <v-btn
+                class="mx-0"
+                dark
+                large
+                :href="'//SteamHub.cn/apps/' + id"
+                target="_black"
+              >
+                前往查看
+              </v-btn>
+            </div>
+            <div class="text-xs-center">
+              Build on <a href="//SteamHub.cn" target="_black">SteamHub.cn</a>
+            </div>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-card>
+  </v-jumbotron>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    layout: 'share',
+    async asyncData ({ params }) {
+      return { id: params.id }
+    },
+    data: () => ({
+      appdetails: ''
+    }),
+    created: function () {
+      axios.get(`https://api.steamhub.cn/api/v1/steam/app/appdetails/` + this.id, {
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        }
+      })
+        .then(response => {
+          this.appdetails = response.data
+        })
+    }
+  }
+</script>
