743605d53e40a9f00c17f9ced34ab58540cfb577
agdholo <agdholo@gmail.com>
2018-08-24 15:32:51 +0800

    delete: Float go-store button

--- components/GameHeader.vue
+++ components/GameHeader.vue
@@ -10,12 +10,6 @@
         <h3>上次更新: {{ lastUpdated | time }}</h3>
       </div>
     </v-flex>
-    <v-tooltip top>
-      <v-btn class="mb-5" fixed dark fab bottom right color="orange" slot="activator" :href="'https://store.steampowered.com/app/' + appID" target="_balck">
-        <v-icon>shop</v-icon>
-      </v-btn>
-      <span>前往 Steam 商店购买</span>
-    </v-tooltip>
   </v-layout>
 </template>
 
