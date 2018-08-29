f99cf21f5fe5fb93fa76d9fbf921f5a9cddba50a
agdholo <agdholo@gmail.com>
2018-08-29 10:23:38 +0800

    add: game list card components add new price element

--- components/GameListCard.vue
+++ components/GameListCard.vue
@@ -2,73 +2,76 @@
   <v-flex d-flex xs12 lg9>
     <v-layout row wrap>
       <v-flex d-flex xs12 sm6 md6 lg4 v-for="(item,i) in list.data" :key="i" class="game-list-card px-3">
-        <v-card v-if="item.AppType" flat class="grey lighten-4">
-          <v-img style="cursor:pointer" :src="'https://cdn.steamstatic.com.8686c.com/steam/apps/' + item.AppID + '/header.jpg'" :lazy-src="'/unknow.jpg'" height="200px" v-on:click="cardTo(item.AppID)">
-            <v-container fill-height fluid pa-2>
-              <v-layout align-start justify-start row fill-height>
-                <v-flex xs12 flexbox class="text-xs-right" v-if="item.app_type">
-                  <v-btn dark small color="grey" class="card-right-icon">
-                    <v-icon left>
-                      {{ item.app_type.DisplayName | typeIcon }}
-                    </v-icon>
-                    {{ item.app_type.DisplayName | typeName }}
-                  </v-btn>
-                  <v-btn class="card-right-attention-icon" small color="primary" dark :loading="dialogAttention" @click.stop="dialogAttention = true">
-                    <v-icon left>
-                      add
-                    </v-icon>
-                    关注
-                  </v-btn>
+        <v-hover>
+          <v-card slot-scope="{ hover }" :class="`elevation-${hover ? 12 : 0}`" flat class="grey lighten-4">
+            <v-img style="cursor:pointer" :src="'https://cdn.steamstatic.com.8686c.com/steam/apps/' + item.AppID + '/header.jpg'" :lazy-src="'/unknow.jpg'" height="200px" v-on:click="cardTo(item.AppID)">
+              <v-container fill-height fluid pa-2>
+                <v-layout align-start justify-start row fill-height>
+                  <v-flex xs12 flexbox class="text-xs-right" v-if="item.app_type">
+                    <v-btn dark small color="grey" class="card-right-icon">
+                      <v-icon left>
+                        {{ item.app_type.DisplayName | typeIcon }}
+                      </v-icon>
+                      {{ item.app_type.DisplayName | typeName }}
+                    </v-btn>
+                    <v-btn class="card-right-attention-icon" small color="primary" dark :loading="dialogAttention" @click.stop="dialogAttention = true">
+                      <v-icon left>
+                        add
+                      </v-icon>
+                      关注
+                    </v-btn>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-img>
+            <v-card-title primary-title class="grey lighten-4">
+              <v-layout row>
+                <v-flex xs9>
+                  <nuxt-link :to="'/apps/'+ item.AppID" style="text-decoration: none;color: #000">
+                    <span class="grey--text">更新于: {{ item.LastUpdated | time }}</span>
+                    <h3 v-if="item.AppType === 0">
+                      未知应用
+                    </h3>
+                    <h3 v-else>
+                      {{ item.Name.slice(0, 20) }}...
+                    </h3>
+                  </nuxt-link>
+                </v-flex>
+                <v-flex xs3 class="text-xs-right">
+                  <v-chip label class="text-xs-right">
+                    <span v-if="item.app_price[0]">
+                      <span v-if="item.app_price[0].PriceFinal">
+                        {{ item.app_price | minAppPrice }} 元
+                      </span>
+                      <span v-else-if="item.app_info[0]">
+                        <span v-if="item.app_info[0].Value === 'prerelease'">
+                          还未发售
+                        </span>
+                        <span v-else>
+                          暂无价格
+                        </span>
+                      </span>
+                      <span v-else>
+                        暂无价格
+                      </span>
+                    </span>
+                    <span v-else-if="item.app_info[0]">
+                      <span v-if="item.app_info[0].Value === 'prerelease'">
+                        还未发售
+                      </span>
+                      <span v-else>
+                        暂无价格
+                      </span>
+                    </span>
+                    <span v-else>
+                      暂无价格
+                    </span>
+                  </v-chip>
                 </v-flex>
               </v-layout>
-            </v-container>
-          </v-img>
-          <v-card-title primary-title class="grey lighten-4">
-            <v-layout row>
-              <v-flex xs9>
-                <nuxt-link :to="'/apps/'+ item.AppID" style="text-decoration: none;color: #000">
-                  <span class="grey--text">更新于: {{ item.LastUpdated | time }}</span>
-                  <h3 class="mb-0">
-                    {{ item.Name.slice(0, 20) }}...
-                  </h3>
-                </nuxt-link>
-              </v-flex>
-              <v-flex xs3 class="text-xs-right" v-if="item.app_type">
-                <v-chip label class="text-xs-right">
-                  {{ item.AppID }}
-                </v-chip>
-              </v-flex>
-            </v-layout>
-          </v-card-title>
-        </v-card>
-
-        <v-card v-else flat class="grey lighten-4">
-          <v-img :lazy-src="'/unknow.jpg'" height="200px">
-            <v-container fill-height fluid pa-2>
-              <v-layout fill-height>
-                <v-flex xs12 align-end flexbox class="text-xs-right">
-                  <v-btn dark small color="grey">
-                    <v-icon left>device_unknown</v-icon> 未知 </v-btn>
-                </v-flex>
-              </v-layout>
-            </v-container>
-          </v-img>
-          <v-card-title primary-title class="grey lighten-4">
-            <v-layout row>
-              <v-flex xs9>
-                <span class="grey--text">更新于: {{ item.LastUpdated | time }}</span>
-                <h3 class="mb-0">
-                  未知应用...
-                </h3>
-              </v-flex>
-              <v-flex xs3 class="text-xs-right">
-                <v-chip label class="text-xs-right">
-                  {{ item.AppID }}
-                </v-chip>
-              </v-flex>
-            </v-layout>
-          </v-card-title>
-        </v-card>
+            </v-card-title>
+          </v-card>
+        </v-hover>
       </v-flex>
     </v-layout>
     <v-dialog v-model="dialogAttention" hide-overlay persistent width="300">
@@ -109,6 +112,8 @@
   import dayjs from 'dayjs'
 
   import 'dayjs/locale/zh-cn'
+  let _ = require('lodash')
+
   dayjs.extend(relativeTime)
 
   export default {
@@ -136,6 +141,19 @@
     filters: {
       time: function (value) {
         return dayjs().locale('zh-cn').from(dayjs(value))
+      },
+      minAppPrice: function (value) {
+        const price = _.minBy(value, 'PriceFinal')
+        if (price.PriceFinal === value[0].PriceFinal) {
+          if (value[0].PriceDiscount !== 0) {
+            if (value[0].PriceDiscount !== value[1].PriceDiscount) {
+              return '史低 ' + value[0].PriceFinal / 100
+            }
+          }
+        } else if (value[0].PriceFinal < value[1].PriceFinal) {
+          return '折扣中 ' + value[0].PriceFinal / 100
+        }
+        return value[0].PriceFinal / 100
       },
       typeName: function (value) {
         switch (value) {
