803f68a93f9139b06928c490b8d6cd4c1029c334
agdholo <agdholo@gmail.com>
2018-09-01 21:17:43 +0800

    create: Bottom Nav component

---
+++ components/BottomNav/Default.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <v-bottom-nav class="hidden-md-and-up" :active.sync="BottomNavIndex" color="white" :value="true" fixed shift>
+      <v-btn v-for="(item, i) in BottomNav.items" :key="i" :to="item.link" @click.stop="LoginDialog = item.click">
+        <span>{{ item.value }}</span>
+        <v-icon>{{ item.icon }}</v-icon>
+      </v-btn>
+    </v-bottom-nav>
+    <LoginDialog :LoginDialog.sync="LoginDialog"></LoginDialog>
+  </div>
+</template>
+
+<script>
+  import LoginDialog from '~/components/Login/Dialog'
+
+  export default {
+    props: ['BottomNav'],
+    components: {
+      LoginDialog
+    },
+    data: () => ({
+      BottomNavIndex: 1,
+      LoginDialog: false
+    }),
+    watch: {
+      BottomNavIndex: function (val) {
+        if (val === 2) {
+          this.BottomNavIndex = 1
+        }
+      }
+    }
+  }
+</script>
