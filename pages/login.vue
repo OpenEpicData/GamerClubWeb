b40a1ca0d618d6132ec42471182b42679157d873
agdholo <agdholo@gmail.com>
2018-09-01 21:18:04 +0800

    create: login page

---
+++ pages/login.vue
@@ -0,0 +1,17 @@
+<template>
+    <LoginDialog :LoginDialog.sync="LoginDialog"></LoginDialog>
+</template>
+
+<script>
+  import LoginDialog from '~/components/Login/Dialog'
+
+  export default {
+    components: {
+      LoginDialog
+    },
+    data: () => ({
+      LoginDialog: true
+    })
+  }
+</script>
+
