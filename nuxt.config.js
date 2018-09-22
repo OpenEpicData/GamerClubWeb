9b358bf61c2f7b68b41a9f0c8d373b3237131b6e
agdholo <agdholo@gmail.com>
2018-09-22 09:45:09 +0800

    update: nuxt version 2.0

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,14 +48,14 @@ module.exports = {
     /*
     ** Run ESLint on save
     */
-    extend (config, ctx) {
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
+    extend (config, { isDev }) {
+      if (isDev && process.client) {
+      config.module.rules.push({
+        enforce: 'pre',
+        test: /\.(js|vue)$/,
+        loader: 'eslint-loader',
+        exclude: /(node_modules)/
+      })
       }
     }
   }
