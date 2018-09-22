56c3c7a0232e8155c3e889938efdc3408d28de35
agdholo <agdholo@gmail.com>
2018-09-22 09:31:13 +0800

    add: vuetify language package

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,7 +1,13 @@
 import '@fortawesome/fontawesome-free/css/all.css'
 import Vue from 'vue'
 import Vuetify from 'vuetify'
+import zhHans from 'vuetify/es5/locale/zh-Hans'
+import en from 'vuetify/es5/locale/en'
 
 Vue.use(Vuetify, {
-  iconfont: 'fa'
+  iconfont: 'fa',
+  lang: {
+    locales: { zhHans, en },
+    current: 'zhHans'
+  }
 })
