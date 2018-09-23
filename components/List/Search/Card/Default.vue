e762592ba32c522f64243387e8dd9cad3e35ffa9
agdholo <agdholo@gmail.com>
2018-09-23 11:11:11 +0800

    create: list of search result component

---
+++ components/List/Search/Card/Default.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <v-layout wrap row>
+      <v-flex xs12 md8 v-for="(item,i) in result.data" :key="i" class="search-list-card">
+        <v-hover>
+          <v-card slot-scope="{ hover }" :class="`elevation-${hover ? 12 : 0}`" v-if="item.AppType" flat class="grey lighten-4"
+            height="18vh">
+            <div>
+              <v-card-title primary-title class="grey lighten-4">
+                <v-layout align-start justify-start row fill-height>
+                  <v-flex>
+                    <nuxt-link :to="$i18n.path('apps/'+ item.AppID)" style="color: #000">
+                      <h3 class="mb-0 display-1">
+                        <span v-if="item.Name && item.ChineseName && language === 'zh-cn'">
+                          {{ item.ChineseName }}
+                        </span>
+                        <span v-else-if="item.Name">{{ item.Name }}</span>
+                      </h3>
+                    </nuxt-link>
+                    <!-- <ve-line :data="chartData[i]" :legend-visible="false"></ve-line> -->
+                    <div>
+                      <span v-for="(item, i) in item.app_info" :key="i">
+                        <span v-if="item.Key === 117">{{ $t('Supported system') }}：{{ item.Value }}</span>
+                      </span>
+                    </div>
+
+                    <span class="grey--text">{{ $t('Updated on') }}: {{ time(item.LastUpdated) }}</span>
+                  </v-flex>
+                  <v-flex flexbox class="text-xs-right" v-if="item.AppType">
+                    <v-btn class="card-right-attention-icon" small color="g-blue-hydrogen" dark>
+                      <v-icon left>
+                        add
+                      </v-icon>
+                      {{ $t('attention') }}
+                    </v-btn>
+                  </v-flex>
+                </v-layout>
+              </v-card-title>
+            </div>
+          </v-card>
+        </v-hover>
+      </v-flex>
+      <v-pagination v-model="result.current_page" :length=result.last_page></v-pagination>
+    </v-layout>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['result'],
+  data: () => ({
+    time: Number
+  })
+}
+</script>
+
+<style>
+  .card-right-attention-icon {
+    display: none;
+  }
+
+  .search-list-card :hover .card-right-icon {
+    display: none;
+  }
+
+  .search-list-card :hover .card-right-attention-icon {
+    display: inline;
+  }
+
+  .v-image__image--preload {
+    filter: blur(0)
+  }
+
+  @media (max-width: 960px) {
+    .card-right-attention-icon {
+      display: inline;
+    }
+
+    .card-right-icon {
+      display: none;
+    }
+  }
+</style>
--- pages/_lang/search/q/_q.vue
+++ pages/_lang/search/q/_q.vue
@@ -1,29 +1,13 @@
 <template>
   <div>
     <div class="grey lighten-4">
-      <v-container
-        fluid
-        grid-list-sm
-        class="index-main-container"
-      >
+      <v-container fluid grid-list-sm class="index-main-container">
         <div class="page-main" id="SearchList">
           <v-content>
-            <v-layout
-              row
-              wrap
-            >
+            <v-layout row wrap>
               <v-flex xs8 md6>
-                <v-text-field
-                  solo
-                  outline
-                  single-line
-                  color="black"
-                  :label="$t('Search game, id') + '...'"
-                  append-icon="search"
-                  v-model="search"
-                  full-width
-                  autofocus
-                ></v-text-field>
+                <v-text-field solo outline single-line color="black" :label="$t('Search game, id') + '...'" append-icon="search"
+                  v-model="search" full-width autofocus></v-text-field>
               </v-flex>
               <v-flex xs4 md4>
                 <v-btn large color="g-blue-hydrogen" dark @click="searchButton">{{ $t('Search now') }}</v-btn>
@@ -35,89 +19,8 @@
                 </h2>
                 <h3 class="ml-3">{{ $t('Query data time') }}: {{ fecthSearchTime }} MS</h3>
               </v-flex>
-              <v-progress-linear
-                indeterminate
-                color="blue"
-                class="mb-0"
-                :height="fecthProgressBarHeight"
-              ></v-progress-linear>
             </v-layout>
-            <v-layout
-              wrap
-              row
-            >
-              <v-flex
-                xs12 md8
-                v-for="(item,i) in result.data"
-                :key="i"
-                class="game-list-card">
-                <v-hover>
-                  <v-card
-                    slot-scope="{ hover }"
-                    :class="`elevation-${hover ? 12 : 0}`"
-                    v-if="item.AppType"
-                    flat
-                    class="grey lighten-4"
-                    height="18vh"
-                  >
-                    <div>
-                      
-                      <v-card-title
-                        primary-title
-                        class="grey lighten-4"
-                      >
-                        <v-layout
-                          align-start
-                          justify-start
-                          row
-                          fill-height
-                        >
-                          <v-flex>
-                            <nuxt-link
-                              :to="$i18n.path('apps/'+ item.AppID)"
-                              style="color: #000"
-                            >
-                              <h3 class="mb-0 display-1">
-                                <span v-if="item.Name && item.ChineseName && language === 'zh-cn'">{{ item.ChineseName }}</span>
-                                <span v-else-if="item.Name">{{ item.Name }}</span>
-                              </h3>
-                            </nuxt-link>
-                            <!-- <ve-line :data="chartData[i]" :legend-visible="false"></ve-line> -->
-                            <div>
-                              <span v-for="(item, i) in item.app_info" :key="i">
-                                <span v-if="item.Key === 117">{{ $t('Supported system') }}：{{ item.Value }}</span>
-                              </span>
-                            </div>
-
-                            <span class="grey--text">{{ $t('Updated on') }}: {{ time(item.LastUpdated) }}</span>
-                          </v-flex>
-                          <v-flex
-                            flexbox
-                            class="text-xs-right"
-                            v-if="item.AppType"
-                          >
-                            <v-btn
-                              class="card-right-attention-icon"
-                              small
-                              color="g-blue-hydrogen"
-                              dark
-                              :loading="dialogAttention"
-                              @click.stop="dialogAttention = true"
-                            >
-                              <v-icon left>
-                                add
-                              </v-icon>
-                              {{ $t('attention') }}
-                            </v-btn>
-                          </v-flex>
-                        </v-layout>
-                      </v-card-title>
-                    </div>
-                  </v-card>
-                </v-hover>
-              </v-flex>
-              <v-pagination v-model="page" :length=result.last_page></v-pagination>
-            </v-layout>
+            <listSearchCard :result.sync="result"></listSearchCard>
           </v-content>
         </div>
       </v-container>
@@ -130,6 +33,7 @@ import axios from 'axios'
 import dayjs from 'dayjs'
 import 'dayjs/locale/zh-cn'
 import relativeTime from 'dayjs/plugin/relativeTime'
+import listSearchCard from '~/components/List/Search/Card/Default'
 dayjs.extend(relativeTime)
 
 export default {
@@ -139,10 +43,12 @@ export default {
       language: store.state.locale
     }
   },
+  components: {
+    listSearchCard
+  },
   data: () => ({
     dialogAttention: false,
     fecthSearchTime: Number,
-    fecthProgressBarHeight: 6,
     q: String,
     isData: Boolean,
     result: String,
@@ -174,7 +80,6 @@ export default {
       .then(response => {
         let fecthSearchTimeEnd = performance.now()
         this.fecthSearchTime = fecthSearchTimeEnd - fecthSearchTimeStart
-        this.fecthProgressBarHeight = 0
         this.page = response.data.current_page
         if (response.data.length === 0) {
           this.isData = false
@@ -209,31 +114,3 @@ export default {
   }
 }
 </script>
-
-<style>
-  .card-right-attention-icon {
-    display: none;
-  }
-
-  .game-list-card :hover .card-right-icon {
-    display: none;
-  }
-
-  .game-list-card :hover .card-right-attention-icon {
-    display: inline;
-  }
-
-  .v-image__image--preload {
-    filter: blur(0)
-  }
-
-  @media (max-width: 960px) {
-    .card-right-attention-icon {
-      display: inline;
-    }
-
-    .card-right-icon {
-      display: none;
-    }
-  }
-</style>
