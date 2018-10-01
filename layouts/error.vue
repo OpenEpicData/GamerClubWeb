49c57694789435990e65202560827aed002fa8ed
agdholo <agdholo@gmail.com>
2018-10-01 11:26:27 +0800

    change: use i18n in error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="container" style="padding-top: 150px">
-    <h1 v-if="error.statusCode === 404">页面不存在</h1>
-    <h1 v-else>应用发生错误异常</h1>
-    <nuxt-link to="/">首 页</nuxt-link>
+    <h1 v-if="error.statusCode === 404">{{ $t('Page does not exist') }}</h1>
+    <h1 v-else>{{ $t('Application error exception') }}</h1>
+    <nuxt-link :to="$i18n.path('')">{{ $t('Homepage') }}</nuxt-link>
   </div>
 </template>
 
 <script>
-  export default {
-    props: ['error']
-  }
+export default {
+  props: ['error'],
+}
 </script>
