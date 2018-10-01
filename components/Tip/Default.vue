ec47555dcd31c34aa50e2d8465cc1ab22f0f50fd
agdholo <agdholo@gmail.com>
2018-10-01 11:13:14 +0800

    create: tip component

---
+++ components/Tip/Default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="tipCcontainer">
+    <div id="lightblue" class="maincircle show">
+      <div class="show" id="logo">
+        <img src="https://i.loli.net/2018/09/28/5bae0609ea694.png" />
+      </div>
+    </div>
+    <div id="textcontainer" class="show">{{ title }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String
+  },
+  mounted () {
+    setTimeout(() => {
+      document.getElementById('textcontainer').setAttribute('class', 'hide'); document.getElementById('logo').setAttribute('class', 'hide')
+      document.getElementsByClassName('maincircle')[0].setAttribute('class', 'maincircle hide')
+    }, 3000)
+  },
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -4,14 +4,7 @@
     <div class="grey lighten-4 my-5">
       <nuxt />
     </div>
-    <div id="tipCcontainer">
-      <div id="lightblue" class="maincircle show">
-        <div class="show" id="logo">
-          <img src="https://i.loli.net/2018/09/28/5bae0609ea694.png" />
-        </div>
-      </div>
-      <div id="textcontainer" class="show">{{ $t('Welcome, tourists') }}</div>
-    </div>
+    <Tip :title.sync="this.$t('Welcome, tourists')"></Tip>
     <BottomNav :BottomNav.sync="BottomNav"></BottomNav>
   </v-app>
 </template>
@@ -19,11 +12,13 @@
 <script>
 import ToolBar from '~/components/ToolBar/Default'
 import BottomNav from '~/components/BottomNav/Default'
+import Tip from '~/components/Tip/Default'
 
 export default {
   components: {
     ToolBar,
-    BottomNav
+    BottomNav,
+    Tip
   },
   data: () => ({
     ToolBar: {
@@ -51,12 +46,6 @@ export default {
   created () {
     if (this.$store.state.locale === 'zh-cn') this.languge = 'zh-cmn-Hans'
     if (this.$store.state.locale === 'en-us') this.languge = 'en-US'
-  },
-  mounted () {
-    setTimeout(function () {
-      document.getElementById('textcontainer').setAttribute('class', 'hide'); document.getElementById('logo').setAttribute('class', 'hide')
-      document.getElementsByClassName('maincircle')[0].setAttribute('class', 'maincircle hide')
-    }, 3000)
   },
   head () {
     return {
