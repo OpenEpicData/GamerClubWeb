b422d9e102b21bcf3dca167121c1715cbfb211e2
agdholo <agdholo@gmail.com>
2018-10-11 16:08:56 +0800

    fix: Parallax's mobile layout problem

--- components/Parallax/Default.vue
+++ components/Parallax/Default.vue
@@ -1,30 +1,36 @@
 <template>
-  <v-parallax dark :src="imgPath" class="hidden-sm-and-down">
+  <v-parallax dark :src="imgPath">
     <v-container fluid grid-list-sm class="index-main-container">
       <v-layout align-center justify-start row fill-height>
-        <v-flex xs10 lg6 class="px-3 py-5" style="background: linear-gradient(to right, rgba(170,75,107,.7), rgba(59,141,153,.7))">
-          <div class="py-3">
+        <v-flex xs12 lg6 class="px-3 py-5" style="background: linear-gradient(to right, rgba(170,75,107,.7), rgba(59,141,153,.7))">
+          <div class="hidden-sm-and-down">
             <h1 class="display-3">
               {{ $t('carousel.item1.title') }}
             </h1>
-            <h4 class="headline font-weight-thin">
+            <h4 class="headline font-weight-thin py-3">
               "{{ $t('carousel.item1.description') }}"
             </h4>
           </div>
-          <v-flex d-flex xs12>
-            <div>
-              <v-btn dark large flat outline :href="this.$t('carousel.item1.href')" target="_black" class="mx-0">
-                <span>{{ $t('learn-more') }}</span>
-              </v-btn>
-              <v-btn dark large flat class="ml-3" :href="this.$t('carousel.item1.video')" target="_black">
-                <v-icon left>far fa-play-circle</v-icon> <span>{{ $t('Play video') }}</span>
-              </v-btn>
-            </div>
+          <div class="hidden-md-and-up">
+            <h1 class="display-1">
+              {{ $t('carousel.item1.title') }}
+            </h1>
+            <h4 class="subheading font-weight-thin py-3">
+              "{{ $t('carousel.item1.description') }}"
+            </h4>
+          </div>
+          <v-flex d-flex xs6>
+            <v-btn dark flat outline :href="this.$t('carousel.item1.href')" target="_black" class="mx-0">
+              <span>{{ $t('learn-more') }}</span>
+            </v-btn>
+            <v-btn dark flat :href="this.$t('carousel.item1.video')" target="_black"  class="ml-3">
+              <v-icon left>far fa-play-circle</v-icon> <span>{{ $t('Play video') }}</span>
+            </v-btn>
           </v-flex>
           <v-flex d-flex xs3>
-            <v-progress-linear :v-model="12" background-color="g-purple-purplin" color="g-purple-purplin"></v-progress-linear>
+            <v-progress-linear background-color="g-purple-purplin" color="g-purple-purplin" class="hidden-sm-and-down"></v-progress-linear>
           </v-flex>
-          <h2 class="title font-weight-thin">{{ $t('carousel.item1.star') }}</h2>
+          <h2 class="title font-weight-thin hidden-sm-and-down">{{ $t('carousel.item1.star') }}</h2>
         </v-flex>
       </v-layout>
     </v-container>
