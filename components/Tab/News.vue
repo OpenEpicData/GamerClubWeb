a07624ba213e6d60b9a9014da69a82625a71c3ba
agdholo <agdholo@gmail.com>
2018-10-20 22:06:52 +0800

    add: news & evaluation in news component

--- components/Tab/News.vue
+++ components/Tab/News.vue
@@ -9,11 +9,57 @@
           <div class="mt-4" v-if="newsData">
             <h2 class="text-xs-center g-blue-hydrogen-text">
               <a :href="newsData.data[0].Link" target="_black" v-if="i === 0">{{ newsData.data[0].Title }}</a>
+              <a :href="newsOnly.data[0].Link" target="_black" v-else-if="i === 1">{{ newsOnly.data[0].Title }}</a>
+              <a :href="evaluation.data[0].Link" target="_black" v-else-if="i === 2">{{ evaluation.data[0].Title }}</a>
             </h2>
           </div>
-          <v-container fluid class="index-main-container" v-if="newsData && i === 0">
-            <v-list subheader class="grey lighten-4">
+          <v-container fluid class="index-main-container">
+            <v-list subheader class="grey lighten-4" v-if="newsData && i === 0">
               <template v-for="(item, k) in newsData.data">
+                <v-divider inset :key="item.Title"></v-divider>
+                <v-list-tile
+                  :key="item.ID"
+                  avatar
+                  :href="item.Link"
+                  target="_black"
+                  v-if="k !== 0"
+                >
+                  <v-list-tile-avatar v-if="item.Type">
+                    <span v-html="item.Type"></span>
+                  </v-list-tile-avatar>
+                  <v-list-tile-content v-if="item.Title">
+                    <v-list-tile-sub-title v-html="item.Title"></v-list-tile-sub-title>
+                  </v-list-tile-content>
+                  <v-list-tile-action v-if="item.LastUpdated">
+                    <h5 class="grey--text"><span>{{ item.LastUpdated }}</span></h5>
+                  </v-list-tile-action>
+                </v-list-tile>
+              </template>
+            </v-list>
+            <v-list subheader class="grey lighten-4" v-if="newsOnly && i === 1">
+              <template v-for="(item, k) in newsOnly.data">
+                <v-divider inset :key="item.Title"></v-divider>
+                <v-list-tile
+                  :key="item.ID"
+                  avatar
+                  :href="item.Link"
+                  target="_black"
+                  v-if="k !== 0"
+                >
+                  <v-list-tile-avatar v-if="item.Type">
+                    <span v-html="item.Type"></span>
+                  </v-list-tile-avatar>
+                  <v-list-tile-content v-if="item.Title">
+                    <v-list-tile-sub-title v-html="item.Title"></v-list-tile-sub-title>
+                  </v-list-tile-content>
+                  <v-list-tile-action v-if="item.LastUpdated">
+                    <h5 class="grey--text"><span>{{ item.LastUpdated }}</span></h5>
+                  </v-list-tile-action>
+                </v-list-tile>
+              </template>
+            </v-list>
+            <v-list subheader class="grey lighten-4" v-if="evaluation && i === 2">
+              <template v-for="(item, k) in evaluation.data">
                 <v-divider inset :key="item.Title"></v-divider>
                 <v-list-tile
                   :key="item.ID"
@@ -37,9 +83,11 @@
             <v-divider inset :key="item.ID"></v-divider>
           </v-container>
           <div class="mx-5">
-            <v-btn block large color="grey lighten-2 elevation-0" v-if="i === 0">
+            <v-btn block large color="grey lighten-2 elevation-0">
               <h4>阅读更多
-                <span class="g-blue-hydrogen-text">最新资讯</span>
+                <span class="g-blue-hydrogen-text"  v-if="i === 0">最新资讯</span>
+                <span class="g-blue-hydrogen-text"  v-if="i === 1">最新新闻</span>
+                <span class="g-blue-hydrogen-text"  v-if="i === 2">最新评测</span>
               </h4>
               <v-icon right small class="g-blue-hydrogen-text">fas fa-long-arrow-alt-right</v-icon>
             </v-btn>
@@ -59,19 +107,25 @@ export default {
         title: '综合'
       },
       {
-        title: '公告'
+        title: '新闻'
       },
       {
-        title: '攻略'
+        title: '评测'
       }
     ],
-    newsData: null
+    newsData: null,
+    newsOnly: null,
+    evaluation: null,
   }),
   mounted: async function () {
-    let [newsData] = await Promise.all([
+    let [newsData, newsOnly, evaluation] = await Promise.all([
       axios.get('https://rest.steamhub.cn/api/v2/news/lists?size=6'),
+      axios.get('https://rest.steamhub.cn/api/v2/news/lists?size=6&type=新闻'),
+      axios.get('https://rest.steamhub.cn/api/v2/news/lists?size=6&type=评测'),
     ])
     this.newsData = newsData.data
+    this.newsOnly = newsOnly.data
+    this.evaluation = evaluation.data
   }
 }
 </script>
