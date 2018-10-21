4cd9d107ea44477d65d315c8d4460304605dfe81
agdholo <agdholo@gmail.com>
2018-10-21 16:42:00 +0800

    add: fix btn in news switch button

--- pages/_lang/topic/index.vue
+++ pages/_lang/topic/index.vue
@@ -1,24 +1,24 @@
 <template>
-  <div>
-    <div class="grey lighten-4">
-      <v-container fluid grid-list-sm class="index-main-container">
-        <div class="page-main">
-          <v-card flat color="grey lighten-4">
-            <div class="mt-4">
-              <h2 class="text-xs-center subheading">
-                <span v-for="(item,i) in news" :key="i" class="mx-3">
-                  <nuxt-link :to="item.link"  v-if="item.link === $route.path ? active = true : ''"  :class="active ? 'g-blue-hydrogen-text' : ''">
-                    {{ item.title }}
-                  </nuxt-link>
-                  <nuxt-link :to="item.link" v-else class="black--text">{{ item.title }}</nuxt-link>
-                </span>
-              </h2>
-            </div>
-            <nuxt-child/>
-          </v-card>
-        </div>
-      </v-container>
-    </div>
+  <div class="grey lighten-4">
+    <v-card flat style="position: fixed;width: 50%;margin: 2vh auto 0 auto;left:0;right:0;z-index: 2;background-color:transparent">
+      <h2 class="text-xs-center subheading">
+        <v-btn>
+          <span v-for="(item,i) in news" :key="i" class="px-3">
+            <nuxt-link :to="item.link"  v-if="item.link === $route.path ? active = true : ''"  :class="active ? 'g-blue-hydrogen-text' : ''">
+              {{ item.title }}
+            </nuxt-link>
+            <nuxt-link :to="item.link" v-else class="black--text">{{ item.title }}</nuxt-link>
+          </span>
+        </v-btn>
+      </h2>
+    </v-card>
+    <v-container fluid grid-list-sm class="index-main-container">
+      <div class="page-main">
+        <v-card flat color="grey lighten-4">
+          <nuxt-child />
+        </v-card>
+      </div>
+    </v-container>
   </div>
 </template>
 
@@ -48,8 +48,19 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
 a:-webkit-any-link {
   text-decoration: none;
 }
+</style>
+
+<style lang="stylus" scoped>
+@media only screen and (max-width: 959px)
+    .v-card
+        width: 100% !important
+        margin-top: 0 !important
+
+    .container
+        padding-left: 0 !important
+        padding-right: 0 !important
 </style>
