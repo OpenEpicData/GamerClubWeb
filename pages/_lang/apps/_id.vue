3dbfcee5b86e7c2e51e7fffe11a9637291998562
agdholo <agdholo@gmail.com>
2018-10-21 11:03:22 +0800

    fix: change changyan js to https

--- pages/_lang/apps/_id.vue
+++ pages/_lang/apps/_id.vue
@@ -193,7 +193,7 @@ export default {
     var conf = 'prod_73685c1a6d8817e9cf1a1a7cbe125c1f'
     var width = window.innerWidth || document.documentElement.clientWidth
     if (width < 960) {
-      window.document.write('<script id="changyan_mobile_js" charset="utf-8" type="text/javascript" src="http://changyan.sohu.com/upload/mobile/wap-js/changyan_mobile.js?client_id=' + appid + '&conf=' + conf + '"><\/script>')
+      window.document.write('<script id="changyan_mobile_js" charset="utf-8" type="text/javascript" src="https://changyan.sohu.com/upload/mobile/wap-js/changyan_mobile.js?client_id=' + appid + '&conf=' + conf + '"><\/script>')
     } else {
       var loadJs = function (d, a) {
         var c = document.getElementsByTagName('head')[0] || document.head || document.documentElement
@@ -216,7 +216,7 @@ export default {
         }
         c.appendChild(b)
       }
-      loadJs('http://changyan.sohu.com/upload/changyan.js',
+      loadJs('https://changyan.sohu.com/upload/changyan.js',
         function () {
           window.changyan.api.config({
             appid: changyanappid,
