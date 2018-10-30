d973dc3b5133195d6900ac1bbeb993c24daca316
AGD <agdholo@gmail.com>
2018-10-30 17:57:45 +0800

    change: index image view

--- components/Parallax/Default.vue
+++ components/Parallax/Default.vue
@@ -1,8 +1,8 @@
 <template>
-  <v-parallax dark :src="$t('carousel[0].img')">
-    <v-container fluid grid-list-sm class="index-main-container">
-      <v-layout align-center justify-start row fill-height>
-        <v-flex xs12 lg6 class="px-3 py-5" style="background: linear-gradient(to right, rgba(170,75,107,.7), rgba(59,141,153,.7))">
+  <v-img dark :src="$t('carousel[0].img')" :aspect-ratio="16/9" gradient="to top right, rgba(31,31,34,.33), rgba(31,31,34,.33)" max-height="60vh">
+    <v-container fluid grid-list-sm fill-height>
+      <v-layout align-center justify-center row>
+        <v-flex xs12 lg6 class="px-3 py-5 text-xs-center white--text">
           <div class="hidden-sm-and-down">
             <h1 class="display-3">
               {{ $t('carousel[0].title') }}
@@ -19,22 +19,22 @@
               "{{ $t('carousel[0].description') }}"
             </h4>
           </div>
-          <v-flex d-flex xs6>
-            <v-btn dark flat outline :href="this.$t('carousel[0].href')" target="_black" class="mx-0">
-              <span>{{ $t('learn-more') }}</span>
-            </v-btn>
-            <v-btn dark flat :href="this.$t('carousel[0].video')" target="_black"  class="ml-3">
-              <v-icon left>far fa-play-circle</v-icon> <span>{{ $t('Play video') }}</span>
-            </v-btn>
+          <v-flex d-flex xs12>
+            <v-layout align-start justify-center row fill-height>
+              <v-flex xs12>
+                <vs-button color="red" style="width: 25%">
+                  <span>{{ $t('learn-more') }}</span>
+                </vs-button>
+                <vs-button color="white" style="width: 25%" class="black--text">
+                  <span>{{ $t('Play video') }}</span>
+                </vs-button>
+              </v-flex>
+            </v-layout>
           </v-flex>
-          <v-flex d-flex xs3>
-            <v-progress-linear background-color="red" color="red" class="hidden-sm-and-down"></v-progress-linear>
-          </v-flex>
-          <h2 class="title font-weight-thin hidden-sm-and-down">{{ $t('carousel[0].star') }}</h2>
         </v-flex>
       </v-layout>
     </v-container>
-  </v-parallax>
+  </v-img>
 </template>
 
 <script>
