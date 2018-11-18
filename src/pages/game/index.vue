8482f01a5be499cf06019da07895ab05a2f24e1e
AGD <agdholo@gmail.com>
2018-11-18 15:09:23 +0800

    fix: all price tips text

--- src/pages/game/index.vue
+++ src/pages/game/index.vue
@@ -326,7 +326,7 @@ export default {
       let array = val.split(',')
       if (array[1] === '300000') {
         return '大于 150 元'
-      } else if (array[0] === '0' && array[0] === '300000') {
+      } else if (array[0] === '0' && array[1] === '300000') {
         return '全部价格'
       } else {
         return array[0] / 100 + ' - ' + array[1] / 100 + '元'
