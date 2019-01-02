3d0d4538278ec01ad5eefd95d9fcf42a15ca9a73
agdholo <agdholo@gmail.com>
2019-01-02 16:41:25 +0800

    feat: add steamhub logo

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ module.exports = {
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/logo.png' },
       {
         rel: 'stylesheet',
         href:
