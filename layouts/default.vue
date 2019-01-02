d9576e1797b53ebfabafadba0d22005af1e6b1b5
agdholo <agdholo@gmail.com>
2019-01-02 16:49:43 +0800

    chore: add logo in layout page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,28 @@
 <template>
-  <v-app>
-    <v-content>
-      <v-container>
-        <nuxt />
-      </v-container>
-    </v-content>
-  </v-app>
+  <VApp>
+    <VToolbar
+      flat
+      absolute
+      color="transparent"
+      dark
+    >
+      <VToolbarTitle>
+        <nuxt-link to="/">
+          <logo />
+        </nuxt-link>
+      </VToolbarTitle>
+    </VToolbar>
+    <VContent>
+      <nuxt />
+    </VContent>
+  </VApp>
 </template>
+
+<script>
+import logo from "~/components/logo";
+export default {
+  components: {
+    logo,
+  }
+};
+</script>
