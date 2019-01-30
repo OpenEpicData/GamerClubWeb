4e194f64238abc0757fa5563e7f30958b28d39b3
AGD <agdholo@gmail.com>
2019-01-30 20:49:06 +0800

    chore: update vuetify 2.0.0 alpha.3

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,7 @@ module.exports = {
       {
         rel: 'stylesheet',
         href:
-          'https://cdn.jsdelivr.net/npm/vuetify@2.0.0-alpha.2/dist/vuetify.min.css'
+          'https://cdn.jsdelivr.net/npm/vuetify@2.0.0-alpha.3/dist/vuetify.min.css'
       },
       {
         rel: 'stylesheet',
