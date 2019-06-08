dd891f1a53cf9480f8d918edeb7dd25084495c4d
agdholo <agdholo@gmail.com>
2019-06-08 19:40:39 +0800

    fix: fix package bug

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,7 @@ module.exports = {
       {
         rel: 'stylesheet',
         href:
-          'https://cdn.jsdelivr.net/npm/vuetify@2.0.0-alpha.16/dist/vuetify.min.css'
+          'https://cdn.jsdelivr.net/npm/vuetify@2.0.0-beta.1/dist/vuetify.min.css'
       },
       {
         rel: 'stylesheet',
